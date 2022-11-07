ed1a687d5483b0a998cc246aca8a79da3593b535
Rhys Jeffery <18280260+RhysJeffery@users.noreply.github.com>
2022-11-07 16:59:02 +1300

    Formatting Changes

--- schedRolloverKerbKeys.ps1
+++ schedRolloverKerbKeys.ps1
@@ -1,13 +1,35 @@
-# The variable $CloudUser requires UPN of a Global Administrator user (username@companyname.com).
-# The variable $Onpremuser requires a Down-Level Logon Name (DOMAIN\USERNAME). The user can be a domain administrator or a hybrid identity administrator.
+<#
+
+Automated Microsoft Deployment Toolkit Installer
+Scripter: Rhys Jeffery
+Email: Rhys.jeffery@neweratech.com
+Date: 7/11/2022
+
+Orginal:
+https://www.shankuehn.io/post/automatically-roll-over-kerberos-decryption-key-with-aad-seamless-sign-on
+https://github.com/sbkuehn/seamlessSsoAad/blob/main/schedRolloverKerbKeys.ps1
+#>
 
 $CloudUser = 'username@companyname.com'
-$CloudEncrypted = Get-Content "C:\Scripts\azureAd_Encrypted_Password.txt" | ConvertTo-SecureString
-$CloudCred = New-Object System.Management.Automation.PsCredential($CloudUser,$CloudEncrypted)
 $OnpremUser = 'DOMAIN\USERNAME'
-$OnpremEncrypted = Get-Content "C:\Scripts\onPrem_Encrypted_Password.txt" | ConvertTo-SecureString
+
+# DO NOT EDIT BELOW
+
+# Password Storage
+$CloudEncrypted = Get-Content ".\SecureStorage\azureAd_Encrypted_Password.txt" | ConvertTo-SecureString
+$OnpremEncrypted = Get-Content ".\SecureStorage\onPrem_Encrypted_Password.txt" | ConvertTo-SecureString
+
+# Create objects for authencation
+$CloudCred = New-Object System.Management.Automation.PsCredential($CloudUser,$CloudEncrypted)
 $OnpremCred = New-Object System.Management.Automation.PsCredential($OnpremUser,$OnpremEncrypted)
  
-Import-Module 'C:\Program Files\Microsoft Azure Active Directory Connect\AzureADSSO.psd1'
-New-AzureADSSOAuthenticationContext -CloudCredentials $CloudCred
-Update-AzureADSSOForest -OnPremCredentials $OnpremCred
+
+# Authenticate against azure ad & update Azure AD SSO Forest
+try{
+    Import-Module 'C:\Program Files\Microsoft Azure Active Directory Connect\AzureADSSO.psd1'
+    New-AzureADSSOAuthenticationContext -CloudCredentials $CloudCred
+    Update-AzureADSSOForest -OnPremCredentials $OnpremCred
+}catch(){
+
+
+}
